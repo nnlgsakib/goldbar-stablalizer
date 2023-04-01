63d217550842c038835cded77819197792d0f13d
Dr Elite (NLG) <107909889+nnlgsakib@users.noreply.github.com>
2023-04-01 10:34:33 +0600

    update some syntax

--- goldbarb.js
+++ goldbarb.js
@@ -2,9 +2,9 @@ const Web3 = require('web3');
 const axios = require('axios');
 
 const CONTRACT_ABI = require('./abi.json'); // import the ABI JSON file
-const CONTRACT_ADDRESS = '0x1Bdb65c43bB546198fA9576325721eae7B5884bA'; // address of the contract
-const OWNER_ADDRESS = '0x0A1bA9481507648Ed2bffA92731519030Cd89308'; // address of the owner account
-const PRIVATE_KEY = '0x825b935d2b3a6287b54a4e5f31e8d1ac383b02a5cecf040b114b34707a475a3b'; // private key of the owner account
+const CONTRACT_ADDRESS = '0x1Bdb65c43bB546198fA9576325721eae7B5884bA'; 
+const OWNER_ADDRESS = '0x0A1bA9481507648Ed2bffA92731519030Cd89308'; 
+const PRIVATE_KEY = '0x825b935d2b3a6287b54a4e5f31e8d1ac383b02a5cecf040b114b34707a475a3b'; 
 
 async function fetchPaxGoldPrice() {
   const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=pax-gold&vs_currencies=usd');
@@ -14,7 +14,7 @@ async function fetchPaxGoldPrice() {
 
 async function main() {
   console.log('Starting main...');
-  const web3 = new Web3('https://data-seed-prebsc-1-s3.binance.org:8545/'); // or your own provider URL
+  const web3 = new Web3('https://data-seed-prebsc-1-s3.binance.org:8545/'); 
   console.log('Web3 initialized...');
   const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
   console.log('Contract instance created...');
@@ -43,7 +43,7 @@ async function main() {
     console.log('Transaction receipt:', txReceipt);
     console.log('Token price set to PAX Gold price');
 
-    await new Promise(resolve => setTimeout(resolve, 60000)); // wait for 1 minute before fetching the price again
+    await new Promise(resolve => setTimeout(resolve, 60000)); 
   }
 }
 
